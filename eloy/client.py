0ca648a3db31b3b1a31681dddf81720902f7158e
Jayson Bunnell <jaysonbunnell@gmail.com>
2023-11-17 11:37:16 -0700

    Update Client

---
+++ eloy/client.py
@@ -0,0 +1,24 @@
+""" Client Management """
+from dataclasses import dataclass
+
+
+@dataclass
+class Client:
+    control: bool = False
+    age: int = inf
+    server: object
+    renderer: object
+
+    def host(self,port='127.0.0.1'):
+        """ """
+        ...
+        
+    def render(self):
+        """ """
+        ...
+
+def promoteMinion(minion: Client):
+    """Set the passed minion as the master"""
+    ...
+
+
