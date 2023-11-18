679a7bd3f283b954f0bc5ae7d9ebb478abd62e25
Jarod Whiting <WhitingJarod@Gmail.com>
2023-11-18 01:12:01 -0700

    NETCODE LET'S NOT

--- eloy/libp2py/src/lib.rs
+++ eloy/libp2py/src/lib.rs
@@ -10,7 +10,7 @@ use std::error::Error;
 use std::hash::{Hash, Hasher};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use tokio::{io, select};
+use tokio::{io, io::AsyncBufReadExt, select};
 use tracing_subscriber::EnvFilter;
 
 lazy_static! {
@@ -55,7 +55,7 @@ impl Networker {
     }
 }
 #[derive(NetworkBehaviour)]
-struct MyBehavior {
+struct MyBehaviour {
     gossipsub: gossipsub::Behaviour,
     mdns: mdns::tokio::Behaviour,
 }
@@ -74,31 +74,40 @@ async fn start_network() -> Result<(), Box<dyn Error>> {
         )?
         .with_quic()
         .with_behaviour(|key| {
+            // To content-address message, we can take the hash of message and use it as an ID.
             let message_id_fn = |message: &gossipsub::Message| {
                 let mut s = DefaultHasher::new();
                 message.data.hash(&mut s);
                 gossipsub::MessageId::from(s.finish().to_string())
             };
+
+            // Set a custom gossipsub configuration
             let gossipsub_config = gossipsub::ConfigBuilder::default()
-                .heartbeat_interval(Duration::from_secs(10))
-                .validation_mode(gossipsub::ValidationMode::Strict)
-                .message_id_fn(message_id_fn)
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
                 .build()
-                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?;
+                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+            // build a gossipsub network behaviour
             let gossipsub = gossipsub::Behaviour::new(
                 gossipsub::MessageAuthenticity::Signed(key.clone()),
                 gossipsub_config,
             )?;
+
             let mdns =
                 mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
-            Ok(MyBehavior { gossipsub, mdns })
+            Ok(MyBehaviour { gossipsub, mdns })
         })?
         .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
         .build();
 
-    let topic = gossipsub::IdentTopic::new(networker!().network_name.clone());
+    // Create a Gossipsub topic
+    let topic = gossipsub::IdentTopic::new("test-net"); //networker!().network_name.clone());
+                                                        // subscribes to our topic
     swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
+    // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
@@ -110,28 +119,30 @@ async fn start_network() -> Result<(), Box<dyn Error>> {
                 let nw =nw1.as_mut().unwrap();
                 let mut outbound =nw.outbound.drain();
                 while let Some(data) = outbound.next() {
-                    if let Ok(_) =  swarm.behaviour_mut().gossipsub.publish(topic.clone(), data.as_bytes()) {}
+                    if let Err(e) =  swarm.behaviour_mut().gossipsub.publish(topic.clone(), data.as_bytes()) {
+                        networker!().events.enqueue(format!("PublishError;{e:?}"));
+                    }
                 }
-            },
+            }
             event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(MyBehaviorEvent::Gossipsub(gossipsub::Event::Message {
-                    propagation_source: peer_id,
-                    message_id,
-                    message,
-                })) => {
-                    networker!().inbound.enqueue(format!("Message;{};{};{}", peer_id, message_id, String::from_utf8_lossy(&message.data)));
-                },
-                SwarmEvent::Behaviour(MyBehaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
                     for (peer_id, multiaddr) in list {
                         networker!().events.enqueue(format!("Discovered;{};{}", peer_id, multiaddr));
                         swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
                     }
                 },
-                SwarmEvent::Behaviour(MyBehaviorEvent::Mdns(mdns::Event::Expired(list))) => {
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
                     for (peer_id, multiaddr) in list {
                         networker!().events.enqueue(format!("Expired;{};{}", peer_id, multiaddr));
                         swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
                     }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id,
+                    message,
+                })) => {
+                    networker!().inbound.enqueue(format!("Message;{};{};{}", peer_id, message_id, String::from_utf8_lossy(&message.data)));
                 },
                 SwarmEvent::ConnectionEstablished { peer_id, connection_id, endpoint, num_established, concurrent_dial_errors, established_in } => {
                     networker!().events.enqueue(format!("ConnectionEstablished;{peer_id};{connection_id};{num_established}"));
@@ -152,7 +163,7 @@ async fn start_network() -> Result<(), Box<dyn Error>> {
                     }
                     networker!().events.enqueue(format!("OutgoingConnectionError;{connection_id};;{error}"));
                 },
-                SwarmEvent::NewListenAddr { listener_id, address } => {
+                SwarmEvent::NewListenAddr { address, .. } => {
                     networker!().events.enqueue(format!("NewListenAddr;{address}"));
                 },
                 SwarmEvent::ExpiredListenAddr { listener_id, address } => {
